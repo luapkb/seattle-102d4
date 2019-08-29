4f6842f58fdeae34b7568435d1ee5f1c49b53214
Paul Bennett <luapkb@gmail.com>
2019-08-29 16:49:06 -0700

    update

--- class-06/js/add-content.js
+++ class-06/js/add-content.js
@@ -1,18 +1,51 @@
 
-function createtimegreating () {
+function createtimegreating() {
     var today = new Date();
-    var hourNow = prompt ('what hour is it?');
-    var greeting; 
-    
-    if  (hourNow > 18) {
+    var hourNow = today.getHours();
+    var greeting;
+
+    if (hourNow > 18) {
         greeting = 'Good evening!';
-    }   else if (hourNow > 12) {
+    } else if (hourNow > 12) {
         greeting = 'Good afternoon!';
-    }   else if (hourNow >0) {
-        greetiong = 'Good morning!'
-    }   else {
+    } else if (hourNow > 0) {
+        greeting = 'Good morning!'
+    } else {
         greeting = 'Welcome!';
     }
-    
-    document.write('<h3>' + greeting + '</h3>'); 
+    return greeting;
+}
+function showOrder() {
+    var order = prompt('what would you like to order');
+    var item, quantity;
+    var items = '';
+
+    while (order !== 'house' && order !== 'hotel') {
+        order = prompt('please enter house or hotel');
+    }
+    //ask quantity
+    quantity = prompt('how many would you like');
+    quantity = Number(quantity);
+
+    //validate quantity
+    while (isNaN(quantity)) {
+        quantity = prompt('please enter a numaric value..');
+        quantity = Number(quantity);
+    }
+
+    if (order === 'hotel') {
+        item ='hotelphoto'
+        // item = '<img src="images/hotel.png">';
+    } else if (order === 'house') {
+        item = 'housephot0'
+        // item = '<img src="images/house.png">';
+
+        for (var i = 0; i < quantity; i++) {
+            items = items + item;
+        }
+        return items;
+    }
+
+
+
 }
