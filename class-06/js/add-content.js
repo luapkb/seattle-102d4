d3c55315bd5336fdd1cba26953cd784eed9bbb27
Paul Bennett <luapkb@gmail.com>
2019-08-29 11:36:04 -0700

    name

--- class-06/js/add-content.js
+++ class-06/js/add-content.js
@@ -1,16 +1,18 @@
 
-function createtimegreating () {var today = new Date();
-var hourNow = prompt ('what hour is it?');
-var greeting; 
-
-if  (hourNow > 18) {
-    greeting = 'Good evening!';
-}   else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
-}   else if (hourNow >0) {
-    greetiong = 'Good morning!'
-}   else {
-    greeting = 'Welcome!';
+function createtimegreating () {
+    var today = new Date();
+    var hourNow = prompt ('what hour is it?');
+    var greeting; 
+    
+    if  (hourNow > 18) {
+        greeting = 'Good evening!';
+    }   else if (hourNow > 12) {
+        greeting = 'Good afternoon!';
+    }   else if (hourNow >0) {
+        greetiong = 'Good morning!'
+    }   else {
+        greeting = 'Welcome!';
+    }
+    
+    document.write('<h3>' + greeting + '</h3>'); 
 }
-
-document.write('<h3>' + greeting + '</h3>');}
